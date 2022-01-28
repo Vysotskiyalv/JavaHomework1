11ffe6da1d3e2920ef912603f2def49eb676d251
Alexey Vysotskiy <maracrus811@gmail.com>
2022-01-28 13:43:18 +0500

    Report added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int DebtValue = 2000000000;
+        int TransferValue = 500000000;
+        int SumValue = DebtValue+TransferValue;
+        System.out.println(SumValue);
+
+    }
+}
